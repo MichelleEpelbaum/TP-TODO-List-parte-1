a38c947dd22007b4b415725e96d214778fd10d73
MICHELLE TAMAR EPELBAUM <48592419@ort>
2025-04-03 12:15:06 -0300

    segundo commit

--- script.js
+++ script.js
@@ -2,7 +2,34 @@ let tareas=[];
 
 function AgregarTarea()
 {
-    let tarea=document.getElementById("tarea")
+    let tarea=document.getElementById("tarea").value
+
+
+    /* local storage*/
+
+
+
+
     tareas.push(tarea)
+
+
     console.log(tareas)
+
+
+
+    let lista = document.getElementById("lista")
+    
+
+    const newDiv = document.createElement("li");
+
+    // and give it some content
+    const newContent = document.createTextNode(tarea);
+  
+    // add the text node to the newly created div
+    newDiv.appendChild(newContent);
+
+    lista.appendChild(newDiv)
+
+
+
 }
