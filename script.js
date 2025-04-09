9a7f628e787b0a0fdb5aae5f657885079bd6fe58
MICHELLE TAMAR EPELBAUM <48592419@ort>
2025-04-09 10:25:50 -0300

    tercer commit

--- script.js
+++ script.js
@@ -1,35 +1,25 @@
-let tareas=[];
+let tareas = [];
 
-function AgregarTarea()
-{
-    let tarea=document.getElementById("tarea").value
+function AgregarTarea() {
+  let tarea = document.getElementById("tarea").value;
+  localStorage.setItem(Date.now(), tarea);
 
+  tareas.push(tarea);
 
-    /* local storage*/
+  console.log(tareas);
 
+  let lista = document.getElementById("lista");
+  const newDiv = document.createElement("li");
+  newDiv.classList.add("tarea");
+  //crear taag check
+  const check = document.createElement("input");
 
+  //agregar el atributo de tipo con valor checkbox
 
+  check.setAttribute("type", "checkbox");
+  newDiv.appendChild(check);
 
-    tareas.push(tarea)
-
-
-    console.log(tareas)
-
-
-
-    let lista = document.getElementById("lista")
-    
-
-    const newDiv = document.createElement("li");
-
-    // and give it some content
-    const newContent = document.createTextNode(tarea);
-  
-    // add the text node to the newly created div
-    newDiv.appendChild(newContent);
-
-    lista.appendChild(newDiv)
-
-
-
+  const newContent = document.createTextNode(tarea);
+  newDiv.appendChild(newContent);
+  lista.appendChild(newDiv);
 }
